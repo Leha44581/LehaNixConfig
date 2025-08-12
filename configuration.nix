1b6d4126a9b699e0cf468dc68382ed0fd69272f6
leha44581 <leha55481@gmail.com>
2025-08-12 21:35:19 +0300

    Mostly formatting changes, added timezone variable

--- configuration.nix
+++ configuration.nix
@@ -14,78 +14,11 @@ in
 			(import "${vars.home-manager}/nixos")
 		];
 
-	# Bootloader.
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
-
-	networking.hostName = "nixos"; # Define your hostname.
-	# networking.wireless.enable = true;	# Enables wireless support via wpa_supplicant.;
-
-	networking.networkmanager.enable = true; 	# Enable Networking
-
-	time.timeZone = "Europe/Moscow"; 			# Timezone
-
-	i18n.defaultLocale = "en_US.UTF-8";			# Default Language
-
-	i18n.extraLocaleSettings = {
-		LC_ADDRESS = "en_US.UTF-8";
-		LC_IDENTIFICATION = "en_US.UTF-8";
-		LC_MEASUREMENT = "en_US.UTF-8";
-		LC_MONETARY = "en_US.UTF-8";
-		LC_NAME = "en_US.UTF-8";
-		LC_NUMERIC = "en_US.UTF-8";
-		LC_PAPER = "en_US.UTF-8";
-		LC_TELEPHONE = "en_US.UTF-8";
-		LC_TIME = "en_US.UTF-8";
-	};
-	
-	i18n.extraLocales = [						# Extra Languages to install
-		"all"
-	];
-
-	services.xserver.enable = true;				# Enable the X11 windowing system
-
-	services.displayManager.sddm.enable = true;	# Enable the KDE Plasma Desktop Environment.
-	services.desktopManager.plasma6.enable = true;
-
-	# Configure keymap in X11
-	services.xserver.xkb = {
-		layout = "us,ru";
-		variant = "";
-		options = "grp:alt_shift_toggle, shift:breaks_caps";
-	};
-
-	services.printing.enable = true;		# Enable CUPS for printing stuff
-
-	# Enable sound with pipewire.
-	services.pulseaudio.enable = false;
-	security.rtkit.enable = true;
-	services.pipewire = {
-		enable = true;
-		alsa.enable = true;
-		alsa.support32Bit = true;
-		pulse.enable = true;
-		# If you want to use JACK applications, uncomment this
-		#jack.enable = true;
-
-		# use the example session manager (no others are packaged yet so this is enabled by default,
-		# no need to redefine it in your config for now)
-		#media-session.enable = true;
-	};
-
-	# services.xserver.libinput.enable = true;	# Enable for touchpad support
-
 	# Define a user account. Don't forget to set a password with ‘passwd’.
 	users.users.${vars.primaryUser} = {
 		isNormalUser = true;
 		description = "${vars.primaryUser}";
 		extraGroups = [ "networkmanager" "wheel" ];
-
-	# User Specific Packages
-		packages = with pkgs; [
-		#	kdePackages.kate
-		#	thunderbird
-		];
 	};
 
 	home-manager.useUserPackages = true;
@@ -108,9 +41,6 @@ in
 		git.enable = true;				# CLI Version Management Utility
 	};
 
-
-	# Allow unfree packages
-	nixpkgs.config.allowUnfree = true;
 
 	# List packages installed in system profile. To search, run:
 	# $ nix search wget
@@ -205,8 +135,7 @@ in
 
 	# List services that you want to enable:
 
-	# Enable the OpenSSH daemon.
-	services.openssh.enable = true;
+
 
 	# Open ports in the firewall.
 	# networking.firewall.allowedTCPPorts = [ ... ];
@@ -214,12 +143,71 @@ in
 	# Or disable the firewall altogether.
 	# networking.firewall.enable = false;
 
-	# This value determines the NixOS release from which the default
-	# settings for stateful data, like file locations and database versions
-	# on your system were taken. It‘s perfectly fine and recommended to leave
-	# this value at the release version of the first install of this system.
-	# Before changing this value read the documentation for this option
-	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+	time.timeZone = "${vars.primaryUserTimezone}"; 			# Timezone
+
+	i18n.defaultLocale = "en_US.UTF-8";			# Default Language
+
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "en_US.UTF-8";
+		LC_IDENTIFICATION = "en_US.UTF-8";
+		LC_MEASUREMENT = "en_US.UTF-8";
+		LC_MONETARY = "en_US.UTF-8";
+		LC_NAME = "en_US.UTF-8";
+		LC_NUMERIC = "en_US.UTF-8";
+		LC_PAPER = "en_US.UTF-8";
+		LC_TELEPHONE = "en_US.UTF-8";
+		LC_TIME = "en_GB.UTF-8";
+		LC_DATE = "en_GB.UTF-8";
+	};
+
+	i18n.extraLocales = [						# Extra Languages to install
+		"all"
+	];
+
+	services.xserver.enable = true;				# Enable the X11 windowing system
+
+	services.displayManager.sddm.enable = true;	# Enable the KDE Plasma Desktop Environment.
+	services.desktopManager.plasma6.enable = true;
+
+	# Configure keymap in X11
+	services.xserver.xkb = {
+		layout = "us,ru";
+		variant = "";
+		options = "grp:alt_shift_toggle, shift:breaks_caps";
+	};
+
+	# services.xserver.libinput.enable = true;	# Enable for touchpad support
+
+	services.printing.enable = true;			# Enable CUPS for printing stuff
+
+	# Enable sound with pipewire.
+	services.pulseaudio.enable = false;
+	security.rtkit.enable = true;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+		# If you want to use JACK applications, uncomment this
+		#jack.enable = true;
+
+		# use the example session manager (no others are packaged yet so this is enabled by default,
+		# no need to redefine it in your config for now)
+		#media-session.enable = true;
+	};
+
+	nixpkgs.config.allowUnfree = true; 			# Allow unfree packages
+
+	networking.hostName = "nixos"; 				# Define your hostname.
+	# networking.wireless.enable = true;		# Enables wireless support via wpa_supplicant.;
+
+	networking.networkmanager.enable = true; 	# Enable Networking
+
+	boot.loader.systemd-boot.enable = true;		# Bootloader
+	boot.loader.efi.canTouchEfiVariables = true;
+
+	services.openssh.enable = true;				# Enable the OpenSSH daemon.
+
 	system.stateVersion = "${vars.stateVersion}"; # Did you read the comment?
 
 }
--- variables.nix
+++ variables.nix
@@ -3,10 +3,16 @@ let
 in
 
 {
+	# This value determines the NixOS release from which the default
+	# settings for stateful data, like file locations and database versions
+	# on your system were taken. It‘s perfectly fine and recommended to leave
+	# this value at the release version of the first install of this system.
+	# Before changing this value read the documentation for this option
+	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
 	stateVersion = "25.05";
 
 	primaryUser = "leha44581";
-
+	primaryUserTimezone = "Europe/Moscow";
 	primaryUserGit = "leha44581";
 	primaryUserEmail = "leha55481@gmail.com";
 
