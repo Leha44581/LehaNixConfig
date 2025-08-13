0ba917fc5bda5c5f0c6a64b20d2b00caffdd9993
leha44581 <leha55481@gmail.com>
2025-08-13 21:48:50 +0300

    Fixed taskbar settings

--- plasma-manager.nix
+++ plasma-manager.nix
@@ -26,24 +26,26 @@ in
 		};
 
 		# Taskbar settings
-		panels.panel = {
-			height = 40;		# Taskbar height
-			floating = false;	# Stops it from detaching, when no fullscreen apps are opened
-			location = bottom;	# Taskbar location
-			opacity = opaque;	# Taskbar transparency (can be set to opaque, adaptive, translucent)
+		panels = [
+			taskbar = {
+				height = 40;		# Taskbar height
+				floating = false;	# Stops it from detaching, when no fullscreen apps are opened
+				location = bottom;	# Taskbar location
+				opacity = opaque;	# Taskbar transparency (can be set to opaque, adaptive, translucent)
 
-			# Widgets that should appear on the taskbar
-			# Some can be found with "find /nix/store -iname "*plasmoids*" " in the /share/plasma/plasmoids type directories
-			# Specific ones can be found with "find /nix/store/*/share/plasma/plasmoids -iname "*string*""
-			widgets = [
-				"org.kde.plasma.kickoff"
-				#"org.kde.plasma.pager"				# Switch between virtual desktops
-				"org.kde.plasma.icontasks"
-				"org.kde.plasma.marginsseparator"
-				"org.kde.plasma.systemtray"
-				"org.kde.plasma.digitalclock"		# Digital Clock, shows date and time
-				"org.kde.plasma.showdesktop"
-			];
-		};
+				# Widgets that should appear on the taskbar
+				# Some can be found with "find /nix/store -iname "*plasmoids*" " in the /share/plasma/plasmoids type directories
+				# Specific ones can be found with "find /nix/store/*/share/plasma/plasmoids -iname "*string*""
+				widgets = [
+					"org.kde.plasma.kickoff"
+					#"org.kde.plasma.pager"				# Switch between virtual desktops
+					"org.kde.plasma.icontasks"
+					"org.kde.plasma.marginsseparator"
+					"org.kde.plasma.systemtray"
+					"org.kde.plasma.digitalclock"		# Digital Clock, shows date and time
+					"org.kde.plasma.showdesktop"
+				];
+			};
+		];
 	};
 }
