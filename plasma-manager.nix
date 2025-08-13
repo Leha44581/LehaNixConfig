7e767aa9508439a2aff0035301d9f92ad80bd3dd
leha44581 <leha55481@gmail.com>
2025-08-13 21:40:16 +0300

    added taskbar settings to plasma-manager.nix

--- plasma-manager.nix
+++ plasma-manager.nix
@@ -7,8 +7,14 @@ in
 {
 	# This is where custom themes n stuff go
 	home.packages = with pkgs; [
+
+		# Themes
 		catppuccin-kde
 		whitesur-kde
+
+		# Widgets/plasmoids, bunch of stuff
+		kdePackages.plasma-workspace
+		kdePackages.plasma-desktop
 	];
 
 	programs.plasma = {
@@ -16,7 +22,28 @@ in
 		enable = true;
 
 		workspace = {
-			lookAndFeel = "org.kde.breezedark.desktop";
+			lookAndFeel = "org.kde.breezedark.desktop";	# Global theme (see available with "plasma-apply-lookandfeel --list")
+		};
+
+		# Taskbar settings
+		panels.panel = {
+			height = 40;		# Taskbar height
+			floating = false;	# Stops it from detaching, when no fullscreen apps are opened
+			location = bottom;	# Taskbar location
+			opacity = opaque;	# Taskbar transparency (can be set to opaque, adaptive, translucent)
+
+			# Widgets that should appear on the taskbar
+			# Some can be found with "find /nix/store -iname "*plasmoids*" " in the /share/plasma/plasmoids type directories
+			# Specific ones can be found with "find /nix/store/*/share/plasma/plasmoids -iname "*string*""
+			widgets = [
+				"org.kde.plasma.kickoff"
+				#"org.kde.plasma.pager"				# Switch between virtual desktops
+				"org.kde.plasma.icontasks"
+				"org.kde.plasma.marginsseparator"
+				"org.kde.plasma.systemtray"
+				"org.kde.plasma.digitalclock"		# Digital Clock, shows date and time
+				"org.kde.plasma.showdesktop"
+			];
 		};
 	};
 }
