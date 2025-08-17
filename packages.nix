b77dcb6b84d4f40ac851103a2723af873a49375b
leha44581 <leha55481@gmail.com>
2025-08-17 03:31:24 +0300

    Fastfetch riced :3

--- packages.nix
+++ packages.nix
@@ -20,57 +20,112 @@ in
 
 	environment.systemPackages = with pkgs.unstable; [
 
-			# Unstable packages, Latest and Greatest
-			blender			# 3D Art Program
-			freecad			# 3D CAD Program
-			prusa-slicer	# 3D Print Slicer
+		# Unstable packages, Latest and Greatest
+		blender			# 3D Art Program
+		freecad			# 3D CAD Program
+		prusa-slicer	# 3D Print Slicer
 
-			yt-dlp			# CLI tool to download videos/music/etc. off the internet in bulk
-			]
+		yt-dlp			# CLI tool to download videos/music/etc. off the internet in bulk
+		]
 
-			++ (with pkgs; [
+		++ (with pkgs; [
 
-			# Stable packages
-			kdePackages.kcalc	# Calculator
+		# Stable packages
+		kdePackages.kcalc	# Calculator
+		gnome-characters	# Character Picker
+		unipicker			# Another Character Picker
 
-			vlc				# Video and general media player
-			strawberry		# Music player
-			baobab			# Disk Usage Space Analyzer
-			obsidian		# Markdown Note Taking app
+		vlc				# Video and general media player
+		strawberry		# Music player
+		baobab			# Disk Usage Space Analyzer
+		obsidian		# Markdown Note Taking app
 
-			pinta 			# Lightweight Drawing and Image Editing tool
-			gimp3			# Serious Image Editor
+		pinta 			# Lightweight Drawing and Image Editing tool
+		gimp3			# Serious Image Editor
 
-			discord			# Messenger
-			telegram-desktop	# Another messenger
+		discord			# Messenger
+		telegram-desktop	# Another messenger
 
-			steamcmd		# CLI version of steam
+		steamcmd		# CLI version of steam
 
-			amnezia-vpn		# Amnezia VPN Client
+		amnezia-vpn		# Amnezia VPN Client
 
-			librewolf		# Web Browser
+		librewolf		# Web Browser
 
-			prismlauncher	# Minecraft Launcher
-			pufferpanel		# Minecraft Server GUI
+		prismlauncher	# Minecraft Launcher
+		pufferpanel		# Minecraft Server GUI
 
-			deluge			# Torrent Client
+		deluge			# Torrent Client
 
-			filezilla		# GUI FTP/SFTP/FTPS Client
+		filezilla		# GUI FTP/SFTP/FTPS Client
 
-			ksnip			# Screenshot Capture Tool
-			obs-studio		# Video Recording and Live Streaming app
+		ksnip			# Screenshot Capture Tool
+		obs-studio		# Video Recording and Live Streaming app
 
-			gparted			# GUI Disk Partitioning tool
+		gparted			# GUI Disk Partitioning tool
 
-			flatpak			# Flatpak Package Manager
-			flatpak-builder	# Flatpak Compiler
+		flatpak			# Flatpak Package Manager
+		flatpak-builder	# Flatpak Compiler
 
-			fastfetch		# Just like neofetch!
-			rsync			# CLI File Sync tool
-			unixtools.fdisk	# CLI Disk Partitioning tool
-			networkmanager	# CLI-GUI Networking tool
-			htop			# CLI-GUI System Monitor
-			gitui			# CLI-GUI for Git
-			bash			# Scripting Language
-		]);
+		fastfetch		# Just like neofetch!
+		tldr			# Community driven man alternative
+		rsync			# CLI File Sync tool
+		unixtools.fdisk	# CLI Disk Partitioning tool
+		networkmanager	# CLI-GUI Networking tool
+		htop			# CLI-GUI System Monitor
+		gitui			# CLI-GUI for Git
+		bash			# Scripting Language
+
+		#nerd-fonts.m+	# All the nerd fonts :3
+		material-design-icons
+		material-icons
+		material-symbols
+		#nerd-fonts.noto
+		#nerd-fonts.hack
+		#nerd-fonts.tinos
+		#nerd-fonts.lilex
+		#nerd-fonts.arimo
+		#nerd-fonts.agave
+		#nerd-fonts._3270
+		nerd-fonts.ubuntu
+		#nerd-fonts.monoid
+        #nerd-fonts.lekton
+        #nerd-fonts.hurmit
+        #nerd-fonts.profont
+        #nerd-fonts.monofur
+        #nerd-fonts.iosevka
+        #nerd-fonts.hasklug
+        #nerd-fonts.go-mono
+        #nerd-fonts.cousine
+        nerd-fonts.zed-mono
+        nerd-fonts.overpass
+        nerd-fonts.mononoki
+        nerd-fonts.gohufont
+        nerd-fonts.d2coding
+        nerd-fonts._0xproto
+        #nerd-fonts.monaspace
+        nerd-fonts.fira-mono
+        nerd-fonts.fira-code
+        nerd-fonts.blex-mono
+        nerd-fonts.anonymice
+        nerd-fonts.space-mono
+        nerd-fonts.liberation
+        nerd-fonts.im-writing
+        nerd-fonts.heavy-data
+        nerd-fonts.geist-mono
+        nerd-fonts.victor-mono
+        #nerd-fonts.ubuntu-sans
+        #nerd-fonts.ubuntu-mono
+        nerd-fonts.roboto-mono
+        nerd-fonts.intone-mono
+        nerd-fonts.inconsolata
+        nerd-fonts.envy-code-r
+        nerd-fonts.commit-mono
+        nerd-fonts.symbols-only
+        nerd-fonts.martian-mono
+        nerd-fonts.iosevka-term
+        nerd-fonts.adwaita-mono
+        nerd-fonts.terminess-ttf
+        nerd-fonts.open-dyslexic
+	]);
 }
