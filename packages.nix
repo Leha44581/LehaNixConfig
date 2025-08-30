dff7651dcbfaac8c628620453ead189eeb2c7453
leha44581 <leha55481@gmail.com>
2025-08-30 17:04:02 +0300

    added nix-index for real this time

--- packages.nix
+++ packages.nix
@@ -80,6 +80,8 @@ in
 		flatpak			# Flatpak Package Manager
 		flatpak-builder	# Flatpak Compiler
 
+		nix-indes		# Lets you find packages by files they provide! use nix-index and nix-locate 
+
 		fastfetch		# Just like neofetch!
 		tldr			# Community driven man alternative
 		coreutils-full	# GNU Core Utilities, the essentials
