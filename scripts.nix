222b99dff3d0fd7a24f538709e8b24d8853eec70
leha44581 <leha55481@gmail.com>
2025-09-03 13:27:02 +0300

    added a script snippet to update tldr cache every rebuild

--- scripts.nix
+++ scripts.nix
@@ -3,11 +3,18 @@
 	# For some reason, you need to wrap scripts in this writeShellApplication bullshit, to actually run them, i hate this :(
 
 let
-	trim-generations-sh = pkgs.writeShellApplication {
+	trim-generations-sh = pkgs.writeShellApplication {	# Garbage Collector packaging
 		name = "trim-generations-sh";
 		text = (builtins.readFile ./scripts/trim-generations.sh);
 		runtimeInputs = [ pkgs.nix pkgs.bash pkgs.coreutils pkgs.gnused ]; # Packages that provide all the executables/commands used by the script
 	 };
+
+	tldrCacheUpdater = pkgs.writeShellApplication {	# tldr cache updater packaging
+		name = "tldrCacheUpdated";
+		text = '' printf "\n" && tldr -u && printf "\n"  '';	# printf to insert linebreaks before and after the command
+		runtimeInputs = [ pkgs.bash pkgs.tldr ];
+	};
+
 	 vars = import ./variables.nix;
 in
 
@@ -23,6 +30,11 @@ in
 			'';
 
 			deps = [];	# Not package dependecies, other activation scripts go here, that must execute before this one
+		};
+
+		# Update cache for the tldr command
+		tldrCacheUpdater = {
+			text = '' ${lib.getExe tldrCacheUpdater} '';
 		};
 	};
 }
